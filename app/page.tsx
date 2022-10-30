ecf93d638172356814c5e1ce2e14af0579da77f7
Lex Nousagi <alexmiyin@gmail.com>
2022-10-31 01:31:40 +0200

    refactor(types&index): move types to separate file and do not display posts on index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,14 @@
-import { getClient } from '../lib/sanity-server'
-import { postQuery } from '../lib/queries'
-
-export type Post = {
-  _id: string
-  slug: string
-  content: string
-  title: string
-  date: string
-}
-
-const getData = async (preview = false) => {
-  const { post } = await getClient(preview).fetch(postQuery, {
-    slug: 'new-post'
-  })
-  return post
-}
+import Link from 'next/link'
 
 export default async function Home() {
-  const post: Post = await getData()
   return (
-    <div className="px-5 py-10">
-      <div className="text-4xl mb-3">{post.title}</div>
-      <div className="text-xl">{post.content}</div>
-      <div className="text-xl">{new Date(post.date).toLocaleString()}</div>
+    <div className="flex flex-col justify-center">
+      <h1 className="text-4xl text-center">
+        Welcome to Next.js 13 and Sanity demo
+      </h1>
+      <Link href="/posts" className="text-center mt-10">
+        Go to posts
+      </Link>
     </div>
   )
 }
---
+++ lib/types.ts
@@ -0,0 +1,7 @@
+export type Post = {
+  _id: string
+  slug: string
+  content: string
+  title: string
+  date: string
+}
