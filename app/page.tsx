2004c7e6ecf521cab25b2ea85867988379a9cf6a
Lex Nousagi <alexmiyin@gmail.com>
2022-10-30 22:33:26 +0200

    feat(cms): add sanity

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,28 @@
+import { use } from 'react'
+import { getClient } from '../lib/sanity-server'
+import { postQuery } from '../lib/queries'
+export type Post = {
+  _id: string
+  slug: string
+  content: string
+  title: string
+  date: string
+}
+
+const getData = async (preview = false) => {
+  const { post } = await getClient(preview).fetch(postQuery, {
+    slug: 'new-post'
+  })
+  return post
+}
+
 export default function Home() {
-  return <div className="text-9xl">Hello world!</div>
+  const post: Post = use(getData(false))
+  return (
+    <div className="px-5 py-10">
+      <div className="text-4xl mb-3">{post.title}</div>
+      <div className="text-xl">{post.content}</div>
+      <div className="text-xl">{new Date(post.date).toLocaleString()}</div>
+    </div>
+  )
 }
---
+++ sanity.config.ts
@@ -0,0 +1,45 @@
+import { createConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import { markdownSchema } from 'sanity-plugin-markdown'
+
+export default createConfig({
+  name: 'default',
+  title: 'Sanity demo',
+  projectId: 'ireq7z47',
+  dataset: 'production',
+  plugins: [deskTool(), markdownSchema()],
+  schema: {
+    types: [
+      {
+        name: 'post',
+        type: 'document',
+        title: 'Post',
+        fields: [
+          {
+            name: 'title',
+            title: 'Title',
+            type: 'string'
+          },
+          {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+              source: 'title'
+            }
+          },
+          {
+            name: 'content',
+            title: 'Content',
+            type: 'string'
+          },
+          {
+            name: 'date',
+            title: 'Date',
+            type: 'datetime'
+          }
+        ]
+      }
+    ]
+  }
+})
