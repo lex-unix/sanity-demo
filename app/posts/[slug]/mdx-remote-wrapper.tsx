25cdf328a023d6c0ba50fc7de920d4933950599c
Lex Nousagi <alexmiyin@gmail.com>
2022-11-02 23:14:12 +0200

    feat(profile): add profile mdx component

--- app/posts/[slug]/mdx-remote-wrapper.tsx
+++ app/posts/[slug]/mdx-remote-wrapper.tsx
@@ -2,10 +2,26 @@
 
 import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote'
 
-const Test = ({ name }: { name: string }) => <div>Hello {name}</div>
+interface ProfileProps {
+  name: string
+  email: string
+  age: number
+}
+
+const Profile = ({ name, email, age }: ProfileProps) => (
+  <div className="border px-4 py-2 rounded-md w-full max-w-[424px] mx-auto mb-[16px]">
+    <h2 className="text-xl font-bold mb-5">
+      {name} <span className="text-base font-normal">{age}</span>
+    </h2>
+
+    <a href={`mailto:${email}`} className="hover:text-blue-300">
+      {email}
+    </a>
+  </div>
+)
 
 const components = {
-  Test
+  Profile
 }
 
 export default function MDXRemoteWrapper(props: MDXRemoteProps) {
